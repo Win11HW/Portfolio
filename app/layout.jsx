e6108109949831ea1dfeda94620aae93300121fd
Win11HW <153312276+Win11HW@users.noreply.github.com>
2025-10-15 23:09:19 +0200

    new

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import { Inter } from "next/font/google"
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "DevPortfolio",
+  description: "web development",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="dark scroll-smooth">
+      <body className={`${inter.className} bg-gray-950 text-gray-100 antialiased`}>{children}</body>
+    </html>
+  )
+}
