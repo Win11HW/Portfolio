da51cfa0520b1beb14c7418f48a277d599258980
Win11HW <153312276+Win11HW@users.noreply.github.com>
2025-10-20 13:13:10 +0200

    Refactor layout component: move body tag above head and streamline structure

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,14 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark scroll-smooth">
-      <head>
+      <body className={`${inter.className} bg-gray-950 text-gray-100 antialiased`}>
         <Script id="gtm-script" strategy="afterInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KM4HH3WQ');`}</Script>
-      </head>
-      <body className={`${inter.className} bg-gray-950 text-gray-100 antialiased`}>
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-KM4HH3WQ"
